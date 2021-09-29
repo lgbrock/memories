3a3c879d75b93d954149cb46145111dac0f780e1
Logan Brock <logan.g.brock@gmail.com>
2021-09-29 17:01:13 -0400

    got Post to show up on Posts - had to fix reducer/posts

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -5,17 +5,27 @@ import { useSelector } from 'react-redux';
 import Post from './post/Post';
 import useStyles from './styles';
 
-const Posts = ({ setCurrentId }) => {
+const Posts = () => {
 	const posts = useSelector((state) => state.posts);
 	const classes = useStyles();
 
 	console.log(posts);
 
-	return (
-		<>
-			<h1>POSTS</h1>
-			<Post />
-		</>
+	return !posts.length ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			className={classes.container}
+			container
+			alignItems='stretch'
+			spacing={3}
+		>
+			{posts.map((post) => (
+				<Grid key={post._id} item xs={12} sm={6} md={6}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,20 +1,14 @@
 // Used for Redux - Allows for scalability when application grows
 // changed how to export because error was showing on DOM - Page still loaded properly
-const posts = (posts = [], action) => {
+export default (posts = [], action) => {
 	switch (action.type) {
 		case 'FETCH_ALL':
-			return {
-				...posts,
-				posts: action.payload,
-			};
+			return action.payload;
+
 		case 'CREATE':
-			return {
-				...posts,
-				posts: [...posts.posts, action.payload],
-			};
+			return [...posts.posts, action.payload];
+
 		default:
 			return posts;
 	}
 };
-
-export default posts;
